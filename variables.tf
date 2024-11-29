1cfa36b3cc26c2a88971c9482c698fcc5875db7d
AJ Khew <AJ.Khew@gmail.com>
2024-11-29 17:18:15 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  description = "AWS region to deploy the resources"
+  default     = "us-east-1"
+}
 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for the subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  default     = "t2.micro"
+}
