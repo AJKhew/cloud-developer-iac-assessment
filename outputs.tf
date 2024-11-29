f7630ef1263bf00f6e1cb406c125a259ec7eff4b
AJ Khew <AJ.Khew@gmail.com>
2024-11-29 17:18:42 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,12 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "web_instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
 
